c0e439f7b920fe5948f17faf3b1c1221401b0bfa
NockyCZ <63038995+NockyCZ@users.noreply.github.com>
2024-06-19 11:53:33 +0200

    api

--- src/DiscordUtilitiesAPI/Events/DiscordUtilitiesEvent.cs
+++
@@ -1,2 +0,0 @@
-namespace DiscordUtilitiesAPI.Events;
-public interface IDiscordUtilitiesEvent { }
--- src/Modules/ConnectedPlayersRole/Config.cs
+++
@@ -1,9 +0,0 @@
-using CounterStrikeSharp.API.Core;
-using System.Text.Json.Serialization;
-
-namespace ConnectedPlayersRole;
-
-public class Config : BasePluginConfig
-{
-    [JsonPropertyName("Role ID")] public string RoleID { get; set; } = "";
-}
